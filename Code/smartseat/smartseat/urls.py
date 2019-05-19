d6d1174be8b8478b30acc235a3b169bbea39794b
laymankank <42634960+laymankank@users.noreply.github.com>
2019-05-19 19:36:29 +0800

    Add files via upload

--- Code/smartseat/smartseat/urls.py
+++ Code/smartseat/smartseat/urls.py
@@ -15,21 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from smart.views import register,login,index,pwback,pwset, userlogin, userindex, table,userregister, userinformation, userinformation2,userinformation3,alterpassword
+from smart.views import register,login,pwback,pwset, userlogin, userindex,userregister, userinformation, userinformation2,userinformation3,alterpassword,deskstate,check,wexin,er,xiuli,admindeel,shifang
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', index, name='index'),
-    path('login/',login, name='login'),
+    path('', login, name='login'),
     path('register/',register, name='register'),
     path('pwback/',pwback, name='pwback'),
     path('pwset/',pwset, name='pwset'),
     path('userlogin/', userlogin),
     path('userindex/', userindex, name='userindex'),
-    path('table/', table, name='table'),
     path('userregister/', userregister, name='userregister'),
     path('userinformation/', userinformation, name='userinformation'),
     path('userinformation2/', userinformation2, name='userinformation2'),
     path('userinformation3/', userinformation3, name='userinformation3'),
-    path('alterpassword/', alterpassword)
-
+    path('alterpassword/', alterpassword),
+    path('deskstate/', deskstate, name='deskstate'),
+    path('check/', check),
+    path('weixin/', wexin, name='wexin'),
+    path('er/', er),
+    path('xiuli/', xiuli, name='xiuli'),
+    path('admindeel/', admindeel, name='admindeel'),
+    path('shifang/', shifang, name='shifang')
 ]
